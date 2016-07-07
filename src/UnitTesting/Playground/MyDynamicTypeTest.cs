2246d32074b0c55eba06893bfc09cbdccb16ff40
aoancea <alx.oancea@gmail.com>
2016-07-07 22:37:20 +0300

    Emit and save Assembly

--- src/UnitTesting/Playground/MyDynamicTypeTest.cs
+++ src/UnitTesting/Playground/MyDynamicTypeTest.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Reflection;
+using System.Reflection.Emit;
 
 namespace Dynamic.Decorator.UnitTesting.Playground
 {
@@ -7,8 +9,48 @@ namespace Dynamic.Decorator.UnitTesting.Playground
 	public class MyDynamicTypeTest
 	{
 		[TestMethod]
-		public void TestMethod1()
+		public void Build_A_Dynamic_Type()
 		{
+
+			/*
+				public class MyDynamicType
+				{
+					private int m_number;
+
+					public MyDynamicType() : this(42) {}
+					public MyDynamicType(int initNumber)
+					{
+						m_number = initNumber;
+					}
+
+					public int Number
+					{
+						get { return m_number; }
+						set { m_number = value; }
+					}
+
+					public int MyMethod(int multiplier)
+					{
+						return m_number * multiplier;
+					}
+				}
+			 */
+
+			AssemblyName aName = new AssemblyName("DynamicAssemblyExample");
+
+			AssemblyBuilder ab = AppDomain.CurrentDomain.DefineDynamicAssembly(aName, AssemblyBuilderAccess.RunAndSave);
+
+
+
+
+
+
+
+
+
+
+
+			ab.Save(aName.Name + ".dll");
 		}
 	}
 }
