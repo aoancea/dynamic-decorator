eac075adcf909b3af3a16d34a08d77a938a8fab6
aoancea <alx.oancea@gmail.com>
2016-07-07 23:18:22 +0300

    Emit several assembly classes

--- src/UnitTesting/Playground/MyDynamicTypeTest.cs
+++ src/UnitTesting/Playground/MyDynamicTypeTest.cs
@@ -40,18 +40,66 @@ namespace Dynamic.Decorator.UnitTesting.Playground
 
 			AssemblyBuilder ab = AppDomain.CurrentDomain.DefineDynamicAssembly(aName, AssemblyBuilderAccess.RunAndSave);
 
-
 			ModuleBuilder mb = ab.DefineDynamicModule(aName.Name, aName.Name + ".dll");
 
+			CreateTypeWithDefaultConstructor(mb, "MyDynamicTypeWithDefaultConstructor");
 
-
-
-
-
+			CreateTypeWithCustomDefaultConstructor(mb, "MyDynamicTypeWithCustomDefaultConstructor");
 
 
 
 			ab.Save(aName.Name + ".dll");
+		}
+
+
+		public Type CreateTypeWithDefaultConstructor(ModuleBuilder moduleBuilder, string typeName)
+		{
+			TypeBuilder tb = moduleBuilder.DefineType(typeName, TypeAttributes.Public);
+
+			Type t = tb.CreateType();
+
+			return t;
+		}
+
+		public Type CreateTypeWithCustomDefaultConstructor(ModuleBuilder moduleBuilder, string typeName)
+		{
+			TypeBuilder tb = moduleBuilder.DefineType(typeName, TypeAttributes.Public);
+
+			// Define a default constructor that supplies a default value
+			// for the private field. For parameter types, pass the empty
+			// array of types or pass null.
+			ConstructorBuilder ctor = tb.DefineConstructor(MethodAttributes.Public, CallingConventions.Standard, Type.EmptyTypes);
+
+			ILGenerator ctorIL = ctor.GetILGenerator();
+			ctorIL.Emit(OpCodes.Ldarg_0);
+			ctorIL.Emit(OpCodes.Call, typeof(object).GetConstructor(Type.EmptyTypes));
+			ctorIL.Emit(OpCodes.Ret);
+
+			Type t = tb.CreateType();
+
+			return t;
+		}
+	}
+
+	public class MyDynamicType
+	{
+		private int m_number;
+
+		public MyDynamicType() : this(42) { }
+		public MyDynamicType(int initNumber)
+		{
+			m_number = initNumber;
+		}
+
+		public int Number
+		{
+			get { return m_number; }
+			set { m_number = value; }
+		}
+
+		public int MyMethod(int multiplier)
+		{
+			return m_number * multiplier;
 		}
 	}
 }
