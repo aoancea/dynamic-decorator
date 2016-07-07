2643fd1e923792312f26e38654bdf1b1d6c5d85d
aoancea <alx.oancea@gmail.com>
2016-07-07 22:20:06 +0300

    Added unit testing playground zone

---
+++ src/UnitTesting/Playground/MyDynamicTypeTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Dynamic.Decorator.UnitTesting.Playground
+{
+	[TestClass]
+	public class MyDynamicTypeTest
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
