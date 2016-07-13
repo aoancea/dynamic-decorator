14c49da1441400284f2026bf476015c3c25c3f61
aoancea <alx.oancea@gmail.com>
2016-07-13 23:16:21 +0300

    Fix build errors

--- src/UnitTesting/UnitTest1.cs
+++ src/UnitTesting/UnitTest1.cs
@@ -1,14 +1,13 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using NUnit.Framework;
 
 namespace Dynamic.Decorator.UnitTesting
 {
-	[TestClass]
-	public class UnitTest1
-	{
-		[TestMethod]
-		public void TestMethod1()
-		{
-		}
-	}
+    [TestFixture]
+    public class UnitTest1
+    {
+        [TestCase]
+        public void TestMethod1()
+        {
+        }
+    }
 }
