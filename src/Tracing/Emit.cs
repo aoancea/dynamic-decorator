47e7092d5a65c571bcd560852e8c0bf5a2295d77
Alex Oancea <alx.oancea@gmail.com>
2016-08-17 22:14:07 +0300

    feature(tracing): adding Emit class responsible for creating the tracing decorators

---
+++ src/Tracing/Emit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dynamic.Decorator.Tracing
+{
+    public static class Emit
+    {
+        public static Type EmitTimeLogger()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
