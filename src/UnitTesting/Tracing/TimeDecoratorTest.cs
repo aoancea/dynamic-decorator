1d017548f3dfa6380daafa594d8921889d2565f7
Alex Oancea <alx.oancea@gmail.com>
2016-08-17 21:55:55 +0300

    test(tracing): added time decorator test

---
+++ src/UnitTesting/Tracing/TimeDecoratorTest.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace Dynamic.Decorator.UnitTesting.Tracing
+{
+    [TestFixture]
+    public class TimeDecoratorTest
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
