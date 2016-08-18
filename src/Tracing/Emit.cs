1e9714e1993eb5006a8500fe281f00ba12d3003c
Alex Oancea <alx.oancea@gmail.com>
2016-08-18 23:01:06 +0300

    feature(tracing): added decorator model as an example

--- src/Tracing/Emit.cs
+++ src/Tracing/Emit.cs
@@ -4,6 +4,35 @@ namespace Dynamic.Decorator.Tracing
 {
     public static class Emit
     {
+        /*
+        public class Foo_Time_Decorator : IFoo
+        {
+            private readonly IFoo decorated;
+            private readonly ILogger logger;
+
+            public Foo_Time_Decorator(IFoo decorated, ILogger logger)
+            {
+                this.decorated = decorated;
+                this.logger = logger;
+            }
+
+            public string Bar(string text)
+            {
+                Stopwatch sw = new Stopwatch();
+                sw.Start();
+
+                string result = decorated.Bar(text);
+
+                sw.Stop();
+
+                logger.Log(sw);
+
+                return result;
+            }
+        }
+        */
+
+
         public static Type EmitTimeLogger()
         {
             throw new NotImplementedException();
