cb11a950a0b486a5159c2ceb2b55c4d138a80993
Alex Oancea <alx.oancea@gmail.com>
2016-08-19 00:37:09 +0300

    feature(tracing): emitted IFoo decorator and created an instance from it

--- src/Tracing/Emit.cs
+++ src/Tracing/Emit.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Reflection;
+using System.Reflection.Emit;
 
 namespace Dynamic.Decorator.Tracing
 {
@@ -33,9 +35,96 @@ namespace Dynamic.Decorator.Tracing
         */
 
 
-        public static Type EmitTimeLogger()
+        public static Type EmitTimeLogger<TDecorated>()
         {
-            throw new NotImplementedException();
+            AssemblyName aName = new AssemblyName("EmitTimeLogger");
+
+            AssemblyBuilder ab = AppDomain.CurrentDomain.DefineDynamicAssembly(aName, AssemblyBuilderAccess.RunAndSave, AppDomain.CurrentDomain.BaseDirectory);
+
+            ModuleBuilder mb = ab.DefineDynamicModule(aName.Name, aName.Name + ".dll");
+
+            Type loggerType = typeof(ILogger);
+
+            Type type = EmitCustomType(mb, typeof(TDecorated), "Foo_Time_Decorator", loggerType);
+
+            ab.Save(aName.Name + ".dll");
+
+            return type;
+        }
+
+
+
+        private static Type EmitCustomType(ModuleBuilder moduleBuilder, Type decoratedType, string typeName, Type loggerType)
+        {
+            TypeBuilder tb = moduleBuilder.DefineType(typeName, TypeAttributes.Public | TypeAttributes.BeforeFieldInit);
+            tb.AddInterfaceImplementation(decoratedType);
+
+            FieldBuilder fbDecorated = tb.DefineField(TypeFieldName(decoratedType), decoratedType, FieldAttributes.Public); // change to Private
+            FieldBuilder fbLogger = tb.DefineField(TypeFieldName(loggerType), loggerType, FieldAttributes.Public); // change to Private
+
+            Type[] ctorParameterTypes = new Type[] { decoratedType, loggerType };
+
+            ConstructorBuilder ctor = tb.DefineConstructor(MethodAttributes.Public, CallingConventions.Standard, ctorParameterTypes);
+
+            ILGenerator ctorIL = ctor.GetILGenerator();
+            ctorIL.Emit(OpCodes.Ldarg_0);
+            ctorIL.Emit(OpCodes.Call, typeof(object).GetConstructor(Type.EmptyTypes));
+            ctorIL.Emit(OpCodes.Ldarg_0);
+            ctorIL.Emit(OpCodes.Ldarg_1);
+            ctorIL.Emit(OpCodes.Stfld, fbDecorated);
+            ctorIL.Emit(OpCodes.Ldarg_0);
+            ctorIL.Emit(OpCodes.Ldarg_2);
+            ctorIL.Emit(OpCodes.Stfld, fbLogger);
+            ctorIL.Emit(OpCodes.Ret);
+
+
+            string implementedMethodName = "Bar";
+
+            MethodBuilder mbMyMethod = tb.DefineMethod(implementedMethodName, MethodAttributes.Public | MethodAttributes.HideBySig | MethodAttributes.NewSlot | MethodAttributes.Virtual | MethodAttributes.Final, typeof(string), new Type[] { typeof(string) });
+
+            ILGenerator myMethodGetIL = mbMyMethod.GetILGenerator();
+            myMethodGetIL.Emit(OpCodes.Ldarg_0);
+            myMethodGetIL.Emit(OpCodes.Ldarg_1);
+            myMethodGetIL.Emit(OpCodes.Ret);
+
+            tb.DefineMethodOverride(mbMyMethod, decoratedType.GetMethod(implementedMethodName));
+
+
+            //PropertyBuilder pbNumber = tb.DefineProperty("Number", PropertyAttributes.HasDefault, typeof(int), null);
+
+            //MethodAttributes getSetAttr = MethodAttributes.Public | MethodAttributes.SpecialName | MethodAttributes.HideBySig;
+
+            //MethodBuilder mbNumberGetAccessor = tb.DefineMethod("get_Number", getSetAttr, typeof(int), null);
+
+            //ILGenerator numberGetIL = mbNumberGetAccessor.GetILGenerator();
+            //numberGetIL.Emit(OpCodes.Ldarg_0);
+            //numberGetIL.Emit(OpCodes.Ldfld, fbNumber);
+            //numberGetIL.Emit(OpCodes.Ret);
+
+            //MethodBuilder mbNumberSetAccessor = tb.DefineMethod("set_Number", getSetAttr, typeof(void), new Type[] { typeof(int) });
+
+            //ILGenerator numberSetIL = mbNumberSetAccessor.GetILGenerator();
+            //numberSetIL.Emit(OpCodes.Ldarg_0);
+            //numberSetIL.Emit(OpCodes.Ldarg_1);
+            //numberSetIL.Emit(OpCodes.Stfld, fbNumber);
+            //numberSetIL.Emit(OpCodes.Ret);
+
+            //pbNumber.SetGetMethod(mbNumberGetAccessor);
+            //pbNumber.SetSetMethod(mbNumberSetAccessor);
+
+            return tb.CreateType();
+        }
+
+        private static string TypeFieldName(Type type)
+        {
+            if (type.IsInterface)
+            {
+                string typeName = type.Name;
+
+                return string.Concat(typeName.ToLowerInvariant()[1], typeName.Substring(2));
+            }
+
+            throw new NotSupportedException("Dynamic.Decorator currently supports interface implementation only. Class extensions are not supported!");
         }
     }
 }
---
+++ src/UnitTesting/Tracing/EmitTest.cs
@@ -0,0 +1,52 @@
+﻿using Dynamic.Decorator.Tracing;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace Dynamic.Decorator.UnitTesting.Tracing
+{
+    [TestFixture]
+    public class EmitTest
+    {
+        private Mock<ILogger> loggerMock;
+        private Mock<IFoo> fooMock;
+
+        [SetUp]
+        public void SetUp()
+        {
+            loggerMock = new Mock<ILogger>(MockBehavior.Strict);
+            fooMock = new Mock<IFoo>(MockBehavior.Strict);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            loggerMock.VerifyAll();
+            fooMock.VerifyAll();
+        }
+
+        [Test]
+        public void EmitTimeLogger_EmitTypeAndInstantiate_TypeGetsInstantiated()
+        {
+            Type fooType = Emit.EmitTimeLogger<IFoo>();
+
+            ConstructorInfo ctor = fooType.GetConstructor(new Type[] { typeof(IFoo), typeof(ILogger) });
+
+            object customFoo = ctor.Invoke(new object[] { fooMock.Object, loggerMock.Object });
+
+            Assert.IsNotNull(customFoo);
+            Assert.AreEqual(fooMock.Object, customFoo.GetType().GetField("foo").GetValue(customFoo));
+            Assert.AreEqual(loggerMock.Object, customFoo.GetType().GetField("logger").GetValue(customFoo));
+        }
+    }
+
+    internal class Logger : ILogger
+    {
+        public void Log(Stopwatch sw)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
