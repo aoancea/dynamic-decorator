eb11ee7a72b0b181fdb1bf428c244c7ca12f23e6
Alex Oancea <alx.oancea@gmail.com>
2016-08-18 22:57:39 +0300

    test(tracing): added mock classes and tested expected behavior. next we must emit the decorator and use it instead.

--- src/UnitTesting/Tracing/TimeDecoratorTest.cs
+++ src/UnitTesting/Tracing/TimeDecoratorTest.cs
@@ -1,13 +1,93 @@
-﻿using NUnit.Framework;
+﻿using Dynamic.Decorator.Tracing;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Diagnostics;
 
 namespace Dynamic.Decorator.UnitTesting.Tracing
 {
     [TestFixture]
     public class TimeDecoratorTest
     {
-        [Test]
-        public void TestMethod1()
+        private Mock<ILogger> loggerMock;
+        private Mock<IFoo> fooMock;
+
+        private Foo_Time_Decorator foo_Time_Decorator;
+
+        [SetUp]
+        public void SetUp()
         {
+            loggerMock = new Mock<ILogger>(MockBehavior.Strict);
+            fooMock = new Mock<IFoo>(MockBehavior.Strict);
+
+            foo_Time_Decorator = new Foo_Time_Decorator(fooMock.Object, loggerMock.Object);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            loggerMock.VerifyAll();
+            fooMock.VerifyAll();
+        }
+
+        [Test]
+        public void Run_ApplyDecorator_DecoratedIsCalledAndTimeIsLogged()
+        {
+            fooMock.Setup(x => x.Bar("run")).Returns("run").Verifiable();
+
+            loggerMock.Setup(x => x.Log(It.IsAny<Stopwatch>())).Verifiable();
+
+            string result = foo_Time_Decorator.Bar("run");
+
+            Assert.AreEqual("run", result);
+        }
+    }
+
+
+    public interface IFoo
+    {
+        string Bar(string text);
+    }
+
+    internal class Foo : IFoo
+    {
+        public string Bar(string text)
+        {
+            return text;
+        }
+    }
+
+    internal class Foo_Time_Decorator : IFoo
+    {
+        private readonly IFoo decorated;
+        private readonly ILogger logger;
+
+        public Foo_Time_Decorator(IFoo decorated, ILogger logger)
+        {
+            this.decorated = decorated;
+            this.logger = logger;
+        }
+
+        public string Bar(string text)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            string result = decorated.Bar(text);
+
+            sw.Stop();
+
+            logger.Log(sw);
+
+            return result;
+        }
+    }
+
+    internal class ConsoleLogger : ILogger
+    {
+        public void Log(Stopwatch sw)
+        {
+            Console.WriteLine(sw.ElapsedMilliseconds);
         }
     }
 }
