e6bc8c2b913112ca79a94c837af2402d5fbb0feb
Alex Oancea <alx.oancea@gmail.com>
2016-08-29 21:52:51 +0300

    feature(tracing): added type definition classes

---
+++ src/Core/TypeDefinition.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Dynamic.Decorator.Core
+{
+    public class TypeDefinition
+    {
+        public string Name { get; set; }
+
+        public ConstructorDefinition Constructor { get; set; }
+
+        public MethodDefinition[] Methods { get; set; }
+    }
+
+    public class ConstructorDefinition
+    {
+        public MethodParameterDefinition[] Parameters { get; set; }
+    }
+
+    public class MethodDefinition
+    {
+        public string Name { get; set; }
+
+        public Type ReturnType { get; set; }
+
+        public MethodParameterDefinition[] Parameters { get; set; }
+    }
+
+    public class MethodParameterDefinition
+    {
+        public string Name { get; set; }
+
+        public Type Type { get; set; }
+    }
+}
