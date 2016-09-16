dca01ee1701da5968166f056299fb4e7a9aecd67
Alex Oancea <alx.oancea@gmail.com>
2016-09-16 21:48:35 +0300

    test(ambient-transation): added NoAmbientTransactionService

---
+++ src/UnitTesting/Playground/AmbientTransaction/Service/NoAmbientTransactionService.cs
@@ -0,0 +1,47 @@
+﻿using Dynamic.Decorator.UnitTesting.Playground.AmbientTransaction.Entity;
+using Dynamic.Decorator.UnitTesting.Playground.AmbientTransaction.Repository;
+using System;
+
+namespace Dynamic.Decorator.UnitTesting.Playground.AmbientTransaction.Service
+{
+	public class NoAmbientTransactionService
+	{
+		private readonly ISquareRepository squareRepository;
+		private readonly IRectangleRepository rectangleRepository;
+
+		public NoAmbientTransactionService(ISquareRepository squareRepository, IRectangleRepository rectangleRepository)
+		{
+			this.squareRepository = squareRepository;
+			this.rectangleRepository = rectangleRepository;
+		}
+
+		public void InsertSquare(Square square)
+		{
+			squareRepository.Upsert(square);
+		}
+
+		public Square GetSquareByID(Guid id)
+		{
+			return squareRepository.GetByID(id);
+		}
+
+
+		public void InsertRectangle(Rectangle rectangle)
+		{
+			rectangleRepository.Upsert(rectangle);
+		}
+
+		public Rectangle GetRectangleByID(Guid id)
+		{
+			return rectangleRepository.GetByID(id);
+		}
+
+
+		public void InsertSquareAndRectangle(Square square, Rectangle rectangle)
+		{
+			squareRepository.Upsert(square);
+
+			rectangleRepository.Upsert(rectangle);
+		}
+	}
+}
